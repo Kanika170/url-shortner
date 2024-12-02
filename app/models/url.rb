e60bafbf4af617ca49f1628835ee62b427c13d6b
Kanika170 <kanikamatta172000@gmail.com>
2024-12-02 21:58:15 +0530

    Add URL shortening logic to model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,11 @@
 class Url < ApplicationRecord
+  validates :original_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp
+
+  before_create :generate_short_url
+
+  private
+
+  def generate_short_url
+    self.short_url = SecureRandom.hex(3)
+  end
 end
