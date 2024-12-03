8d432295263b2b86abc14fd42ee1f754e0515c81
Kanika170 <kanikamatta172000@gmail.com>
2024-12-03 14:49:46 +0530

    RSwag configurations, add Swagger API documentation

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem "rspec-rails"
+  gem "rswag-specs"
 end
 
 group :development do
@@ -65,5 +67,8 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
-gem 'rspec-rails'
 gem 'rswag'
+gem 'rswag-api'
+gem 'rswag-ui'
+gem 'byebug'
+gem 'dotenv', groups: [:development, :test]
--- config/initializers/rswag_api.rb
+++ config/initializers/rswag_api.rb
@@ -1,14 +1,6 @@
-Rswag::Api.configure do |c|
+# config/initializers/rswag_api.rb
 
-  # Specify a root folder where Swagger JSON files are located
-  # This is used by the Swagger middleware to serve requests for API descriptions
-  # NOTE: If you're using rswag-specs to generate Swagger, you'll need to ensure
-  # that it's configured to generate files in the same folder
-  c.openapi_root = Rails.root.to_s + '/swagger'
-
-  # Inject a lambda function to alter the returned Swagger prior to serialization
-  # The function will have access to the rack env for the current request
-  # For example, you could leverage this to dynamically assign the "host" property
-  #
-  #c.swagger_filter = lambda { |swagger, env| swagger['host'] = env['HTTP_HOST'] }
+Rswag::Api.configure do |config|
+  # Specify the root folder where Swagger files are generated
+  config.openapi_root = Rails.root.join('public', 'api-docs').to_s
 end
